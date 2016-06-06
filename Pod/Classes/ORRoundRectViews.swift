1b713ec742d77d7f7683df20159eff398c95fea5
Maxim Soloviev <maxim@omega-r.com>
2016-06-06 20:34:50 +0300

    ORRoundRectView was added

--- Pod/Classes/ORRoundRectViews.swift
+++ Pod/Classes/ORRoundRectViews.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+public class ORRoundRectView : UIView {
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = bounds.height / 2
+    }
+}
+
 public class ORRoundRectButton : UIButton {
     
     override public func layoutSubviews() {
