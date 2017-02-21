b92fc062d2d1b239e28b5213196fc477d960f026
Evgeny Ivanov <evgeny.ivanov@omega-r.com>
2017-02-21 18:32:06 +0300

    ORCustomContentButton was updated

--- Pod/Classes/ORCustomContentButton.swift
+++ Pod/Classes/ORCustomContentButton.swift
@@ -10,16 +10,19 @@ import UIKit
 
 open class ORCustomContentButton: UIControl {
 
+    var defaultAlpha: CGFloat = 1.0
+    
     open override func awakeFromNib() {
         super.awakeFromNib()
         
         isExclusiveTouch = true
+        defaultAlpha = alpha
     }
     
     open override var isHighlighted: Bool {
         didSet {
             UIView.animate(withDuration: 0.1, animations: { _ in
-                self.alpha = self.isHighlighted ? 0.3 : 1.0
+                self.alpha = self.isHighlighted ? 0.3 : self.defaultAlpha
             }) 
         }
     }
