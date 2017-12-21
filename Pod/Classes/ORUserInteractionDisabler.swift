b0b4679bbd522194715a32449bed29834d60436f
Maxim Soloviev <maxim@omega-r.com>
2017-12-21 21:00:32 +0300

    3.7.5: isTurnedOff property added to ORUserInteractionDisabler to turn off the Disabler for testing purposes

--- Pod/Classes/ORUserInteractionDisabler.swift
+++ Pod/Classes/ORUserInteractionDisabler.swift
@@ -14,17 +14,24 @@ open class ORUserInteractionDisabler {
     open static let shared = ORUserInteractionDisabler()
     
     fileprivate var enablingTimer: Timer?
-
+    
     fileprivate init() {
     }
     
+    /// use to turn off the Disabler for testing purposes
+    open var isTurnedOff = false
+    
     open func disableInteractions(onTime duration: TimeInterval) {
+        guard !isTurnedOff else { return }
+        
         UIApplication.shared.beginIgnoringInteractionEvents()
         
         enablingTimer = Timer.scheduledTimer(timeInterval: duration, target: self, selector: #selector(enableInteractions), userInfo: nil, repeats: false)
     }
     
     @objc open func enableInteractions() {
+        guard !isTurnedOff else { return }
+        
         enablingTimer?.invalidate()
         enablingTimer = nil
         if UIApplication.shared.isIgnoringInteractionEvents {
