58dc457b1a368d160180c18116d2b437e88513a5
Sergey Aleksandrov <sergey.aleksandrov@omega-r.com>
2019-11-08 17:32:58 +0300

    Updated UiView+nib: added generic type. Example project migrated to swift 5

--- Pod/Classes/UIView+Nib.swift
+++ Pod/Classes/UIView+Nib.swift
@@ -11,20 +11,20 @@ import PureLayout
 
 extension UIView {
     
-    public static func or_loadFromNib() -> UIView {
+    public static func or_loadFromNib() -> Self {
         return or_loadFromNib(owner: nil)
     }
     
-    public static func or_loadFromNib(owner: AnyObject?) -> UIView {
+    public static func or_loadFromNib(owner: AnyObject?) -> Self {
         let v = Bundle(for: self.classForCoder()).loadNibNamed(String(describing: self), owner: owner, options: nil)!.first
-        return v as! UIView
+        return v as! Self
     }
     
-    public static func or_loadFromNib(containerToFill: UIView) -> UIView {
+    public static func or_loadFromNib(containerToFill: UIView) -> Self {
         return or_loadFromNib(containerToFill: containerToFill, owner: nil)
     }
     
-    public static func or_loadFromNib(containerToFill: UIView, owner: AnyObject?) -> UIView {
+    public static func or_loadFromNib(containerToFill: UIView, owner: AnyObject?) -> Self {
         let v = or_loadFromNib(owner: owner)
         v.translatesAutoresizingMaskIntoConstraints = false
         containerToFill.addSubview(v)
